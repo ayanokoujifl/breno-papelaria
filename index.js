fe61c093922fce4c0ba1b522cdbb8b1d37ccf070
ayanokoujifl <guleite3@gmail.com>
2024-12-19 11:15:09 -0300

    input mask adjusted

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename)
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({ origin: "*" }))
 app.use("/clientes", clienteRoutes)
 app.use("/funcionarios", funcionarioRoutes)
 app.use("/fornecedores", fornecedoresRoutes)
